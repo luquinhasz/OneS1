882c9e8ca8cb698527be0d86b3ccd0382b14717d
luquinhasz <99339399+luquinhasz@users.noreply.github.com>
2022-03-07 08:36:47 -0300

    Add files via upload

---
+++ aula05/copy_of_mighty_duup_waasa1.ino
@@ -0,0 +1,70 @@
+const int led1 = 6;
+const int led2 = 7;
+const int led3 = 8;
+const int led4 = 9;
+const int led5 = 10;
+
+const int pot = 0;
+int var = 0;
+
+ void setup()
+ { 
+
+      pinMode(led1, OUTPUT);
+      pinMode(led2, OUTPUT ); 
+      pinMode(led3, OUTPUT ); 
+      pinMode(led4, OUTPUT ); 
+      pinMode(led5, OUTPUT ); 
+       Serial.begin(9600);
+ }
+
+ void loop()
+ {
+   var = analogRead(pot);
+   Serial.println(var);
+   delay(200);
+
+   if (var > 50){
+digitalWrite(led1, HIGH);
+   }
+  
+   else {
+digitalWrite(led1, LOW);
+ 
+  }
+
+if (var > 200){
+digitalWrite(led2, HIGH);
+   }
+  
+   else {
+digitalWrite(led2, LOW);
+ 
+  }
+
+if (var > 400){
+digitalWrite(led3, HIGH);
+   }
+  
+   else {
+digitalWrite(led3, LOW);
+ 
+  }
+
+if (var > 600){
+digitalWrite(led4, HIGH);
+   }
+  
+   else {
+digitalWrite(led4, LOW);
+ 
+  }
+
+if (var > 900){
+digitalWrite(led5, HIGH);
+   }
+  
+   else {
+digitalWrite(led5, LOW);
+  }
+ }
